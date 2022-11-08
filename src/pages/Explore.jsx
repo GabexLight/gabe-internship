2443a50a2190c4040252101e16d6cc9fd97f3805
GabexLight <gabevancoonis@aol.com>
2022-11-08 10:57:02 -0500

    added animations to everyhting that needed it

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,12 +2,10 @@ import React, { useEffect } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 
-//  https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections
-
 const Explore = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
-    // Does this work?
+
   }, []);
 
   return (
